9006a2ec19eaa9d0ee8db42da0f7d2824e54aa77
MD. Tanvir <mt14050@gmail.com>
2022-07-19 18:23:28 +0600

    2nd commit

--- src/pages/Shared/Navber.js
+++ src/pages/Shared/Navber.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Navber = () => {
   return (
     <div>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabindex="0" className="btn btn-ghost lg:hidden">
+      <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                class="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -26,52 +25,58 @@ const Navber = () => {
             </label>
             <ul
               tabindex="0"
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
-              </li>
-              <li tabindex="0">
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link to="/">HOME</Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li>
+                <Link to="/appointment">APPOIENMENT</Link>
+              </li>
+              <li>
+                <Link to="/reviews">REVIEWS</Link>
+              </li>
+              <li>
+                <Link to="/contact">CONTACT US</Link>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">
             <li>
-              <Link to='/'>Home</Link>
+              <Link className="text-lg" to="/">
+                HOME
+              </Link>
             </li>
             <li>
-              <Link to='/about'>about</Link>
+              <Link className="text-lg" to="/about">
+                ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link className="text-lg" to="/appointment">
+                APPOIENMENT
+              </Link>
+            </li>
+            <li>
+              <Link className="text-lg" to="/reviews">
+                REVIEWS
+              </Link>
+            </li>
+            <li>
+              <Link className="text-lg" to="/contact">
+                CONTACT US
+              </Link>
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Get started</a>
+        <div class="navbar-end">
+          <a class="btn">LOGIN</a>
         </div>
       </div>
     </div>
